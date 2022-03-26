0d31a62c297dd3581d38d5136dff8113514a68c7
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-26 21:51:34 +0600

    adding some info

--- src/components/Answers/Answers.js
+++ src/components/Answers/Answers.js
@@ -5,15 +5,17 @@ import React from 'react';
 const Answers = () => {
     return (
         <div className='writing'>
-            <h2>Questions and Answers?</h2>
-            <h4>How does React works</h4>
-            <p>Answers: React is a frameWork of JavaScript which helps to build user interfaces. It very effective tools to make single page applications and mobile supported apps.React can build complex apps in very effectively using declarative code.
+            <h1 className='header'> Questions and Answers</h1>
+            <h2>1. How does React works?</h2>
+            <p><span>Answers:</span> React is a frameWork of JavaScript which helps to build user interfaces. It very effective tools to make single page applications and mobile supported apps.React can build complex apps in very effectively using declarative code.
                 <br /><br />
             React application is made with multiple components. the component renders a small, reusable HTML. the components are nested in other components to operate complex application to be built very easily. <br /><br />
             React generale a virtual DOM which is basically a DOM tree representation in Js.it uses the virtual representation when it needs to read or write to the DOM. then Virtual DOM tries updata the browser's DOM very efficiently as possible. 
-            unlike borwser's DOM elements,React elements are plain objects and very easy to create. React virtual DOM update DOM to match the react element. this is the reason, JS is very fast to update the DOM elements and speed up its manipulation.           <h2>What is the different between props and state?</h2>
-            <p>Answer: </p>
-            <h4>there are some difference between props and state as below:</h4>
+            unlike borwser's DOM elements,React elements are plain objects and very easy to create. React virtual DOM update DOM to match the react element. this is the reason, JS is very fast to update the DOM elements and speed up its manipulation. 
+            <br/> <br/>       
+            <h2>2. What is the different between props and state?</h2>
+            
+            <h4>Answer: there are some difference between props and state as below:</h4>
             <span>Immutable/Mutable:</span> <br/>
             props can't be modified or changed. it only can show the data to UI. on the other hand state is mutable. Here data can be changed. <br/>
             <span>Passing data between components: </span>
